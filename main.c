107ed11f455bb1379281a74c2393501c7d5b897b
2021t01252 <168732555+2021t01252@users.noreply.github.com>
2024-05-25 12:42:08 +0530

    filehandlecalculator

--- main.c
+++ main.c
@@ -42,3 +42,41 @@ main() {
 
     return 0;
 }
+
+
+void DisplaySavedNotations() {
+
+    char back_btn;
+    FILE *file = fopen("Reverse_Polish_Notation.txt", "r"); // Open the file for reading
+    if (file == NULL) {
+        printf("Unable to open file or file does not exist.\n");
+        return;
+    }
+
+    char line[200]; // Buffer to hold each line read from the file
+    printf("Saved Reverse Polish Notation and Result\n\n");
+    printf("-------------------------------------------------------------------\n");
+    printf("| REVERSE POLISH NOTATION \t | \t RESULT \t\t  |\n");
+    printf("-------------------------------------------------------------------\n");
+    while (fgets(line, sizeof(line), file) != NULL) {
+        printf("%s", line); // Print each line read from the file
+        printf("-------------------------------------------------------------------\n");
+    }
+
+    fclose(file); // Close the file after reading
+
+    printf("\n<-- If You Want To Go Back (Y/N) : ");
+    scanf("%s",&back_btn);
+
+    if(back_btn == 'Y' || back_btn == 'y')
+    {
+        system("cls");
+        main();
+    }
+    else
+    {
+        system("cls");
+        Exit_Message();
+        exit(0);
+    }
+}
